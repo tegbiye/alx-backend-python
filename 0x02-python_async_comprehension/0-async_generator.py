7975d35909c443f6f740b8f92d4b243382c82e8b
tegbiye <se.tegbabu.solomon@gmail.com>
2024-08-20 12:53:41 +0000

    fixed error

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-
+"""Creates a generator"""
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """Each time asynchronously waits 1 second,
         then yield a random number between 0 and 10"""
     for _ in range(10):
